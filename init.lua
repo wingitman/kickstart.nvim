00b73580b76d43c016ec309b1541582078ce49d1
David <david.blackburn@justaccounts.com>
2025-05-23 14:53:53 +0100

    formatting changes

--- init.lua
+++ init.lua
@@ -757,7 +757,7 @@ require('lazy').setup({
         -- Disable "format_on_save lsp_fallback" for languages that don't
         -- have a well standardized coding style. You can add additional
         -- languages here or re-enable it for the disabled ones.
-        local disable_filetypes = { c = true, cpp = true }
+        local disable_filetypes = { c = true, cpp = true, json = true }
         if disable_filetypes[vim.bo[bufnr].filetype] then
           return nil
         else
@@ -770,7 +770,7 @@ require('lazy').setup({
       formatters_by_ft = {
         lua = { 'stylua' },
         rust = { 'ast-grep' },
-        cs = { 'clang-format' },
+        cs = { 'csharpier', 'ast-grep', 'clang-format' },
         -- Conform can also run multiple formatters sequentially
         python = { 'isort', 'black' },
         sql = { 'sqlls' },
